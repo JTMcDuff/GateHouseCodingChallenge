98220b28dedec0936a7f80b64e1e19d1ade9c3e9
JTMcDuff <jtmcduff@gmail.com>
2017-07-21 12:43:16 -0500

    Successfully connected to mysql with php.

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+  /* Obviously root and no password isn't viable for production, but this is a local test. */
+
+  $link = mysqli_connect("127.0.0.1", "root","", "gatehouse");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+//echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+//echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
